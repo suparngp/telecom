4ea7a65e6ad45b56977aadcdc0a249b3d2edaf72
eogus618 <eogus618@naver.com>
2013-10-11 21:38:08 -0500

    Added GSON Example
    Signed-off-by: eogus618 <eogus618@naver.com>

---
+++ VirtualPhone/src/com/example/messages/DemoRunner.java
@@ -0,0 +1,15 @@
+package com.example.messages;
+
+public class DemoRunner {
+	public static void main(String[] args){
+		/**
+		 * Gson gson = new Gson();
+		 * Sting input; // from the server
+		 * Hashmap
+		 * GetContactsAndroid req = gson.parse(input, GetContactsAndroid.class);
+		 * 
+		 * req.getTimeStamp()
+		 * */
+		
+	}
+}
---
+++ VirtualPhone/src/com/example/messages/GetContactsAndroid.java
@@ -0,0 +1,18 @@
+package com.example.messages;
+
+public class GetContactsAndroid {
+	private String type;
+	private long timestamp;
+	String getType() {
+		return type;
+	}
+	void setType(String type) {
+		this.type = type;
+	}
+	long getTimestamp() {
+		return timestamp;
+	}
+	void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+}
