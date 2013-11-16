7a40c6cde5c6aec43c40f6e7990bc1f388f43740
Suparn Gupta <gupta.suparn@gmail.com>
2013-11-16 01:06:11 -0600

    Changed to app1.js

--- node/app1.js
+++ node/app1.js
@@ -51,7 +51,7 @@ io.sockets.on('connection', function (socket) {
         }
     });
 
-    socket.emit('news', { hello: 'world' });
+    //socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
         console.log(data);
     });
